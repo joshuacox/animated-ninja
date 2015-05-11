10ec85b53c7472c16f9c7ce92dffbc803b8f9d8b
Josh Cox <josh@webhosting.coop>
2015-05-11 10:37:56 -0500

    redmine and friends

--- monday.sh
+++ monday.sh
@@ -7,3 +7,15 @@ sed -i "s/^ENV\s*DDP_updated\(.*\)$/ENV\ DDP_updated\ $DATE/" Dockerfile
 git commit -am "Automatically Refreshed on $DATE"
 git push
 #grep -P '^ENV\s*DDP_updated\s*\d*$' /mnt/xyliss/git/DDP/Dockerfile
+
+# Ubuntu NGINX
+cd /mnt/xyliss/git/ubuntu-nginx/
+sed -i "s/^ENV\s*UBUNTU_NGINX_UPDATED\(.*\)$/ENV\ UBUNTU_NGINX_UPDATED\ $DATE/" Dockerfile
+git commit -am "Automatically Refreshed on $DATE"
+git push
+
+# Ubuntu 
+cd /mnt/xyliss/git/docker-ubuntu/
+sed -i "s/^ENV\s*DOCKER_UBUNTU_UPDATED\(.*\)$/ENV\ DOCKER_UBUNTU_UPDATED\ $DATE/" Dockerfile
+git commit -am "Automatically Refreshed on $DATE"
+git push
--- tuesday.sh
+++ tuesday.sh
@@ -6,3 +6,9 @@ sed -i "s/^ENV\s*DDD_updated\(.*\)$/ENV\ DDD_updated\ $DATE/" Dockerfile
 git commit -am "Automatically Refreshed on $DATE"
 git push
 # grep -P '^ENV\s*DDD_updated\s*\d*$' Dockerfile
+
+# DOCKER_REDMINE
+cd /mnt/xyliss/git/docker-redmine/
+sed -i "s/^ENV\s*DOCKER_REDMINE_UPDATED\(.*\)$/ENV\ DOCKER_REDMINE_UPDATED\ $DATE/" Dockerfile
+git commit -am "Automatically Refreshed on $DATE"
+git push
